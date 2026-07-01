c4fc3ddf037ae3ff83c31eecbc93b0d8ef3e2d20
Martian <martdudn@icloud.com>
2026-07-01 21:22:46 +0200

    blendet-fork

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,14 +1,52 @@
+import type React from "react";
 import styled from "./Modal.module.css";
+import { createPortal } from "react-dom";
+import { useEffect } from "react";
 
-export default function Modal() {
-  return (
-    <div className={styled.backdrop} role="dialog" aria-modal="true">
+interface FormProps {
+  children: React.ReactNode;
+  onClose: () => void;
+}
+
+export default function Modal({ children, onClose }: FormProps) {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    const handleKeydown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeydown);
+    return () => {
+      document.body.style.overflow = "";
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, [onClose]);
+  const handleBackdropModal = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  return createPortal(
+    <div
+      className={styled.backdrop}
+      role="dialog"
+      aria-modal="true"
+      onClick={handleBackdropModal}
+    >
       <div className={styled.modal}>
-        <button className={styled.closeButton} aria-label="Close modal">
+        <button
+          className={styled.closeButton}
+          aria-label="Close modal"
+          onClick={onClose}
+        >
           &times;
         </button>
-        {/* children */}
+        {children}
       </div>
-    </div>
+    </div>,
+    document.body,
   );
 }
